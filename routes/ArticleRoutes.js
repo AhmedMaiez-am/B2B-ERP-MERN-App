7527831cf0456eefd644d4d027fb6c07b2fdb433
AhmedMaiez-am <ahmeddhiaeddine.maiez@esprit.tn>
2023-02-16 09:50:45 +0100

    connecting to BS api

--- routes/ArticleRoutes.js
+++ routes/ArticleRoutes.js
@@ -1,31 +1,35 @@
 const express = require("express");
 const axios = require("axios");
 const router = express.Router();
-const Article = require("../models/Articles.js");
+const ntlm = require("express-ntlm");
+//const Article = require("../models/Articles.js");
 
+// Define auth function
+const auth = ntlm({
+  debug: function () {
+    const args = Array.prototype.slice.apply(arguments);
+    console.log.apply(null, args);
+  },
+  domain: "DESKTOP-1EF91E4",
+  username: "ahmed",
+  password: "ahmed2022",
+});
 
 // Get customer data from Business Central
-async function getArticlesFromBC(ntlm) {
-  const companyId = 'CRONUS France S.A.';
+async function getArticlesFromBC() {
+  const companyId = "CRONUS France S.A.";
   const encodedCompanyId = encodeURIComponent(companyId);
   const url = `http://desktop-1ef91e4:7048/BC210/ODataV4/Company('${encodedCompanyId}')/ItemListec`;
-  const options = {
-    auth: ntlm
-  };
-  const response = await axios.get(url, options);
+  const response = await axios.get(url);
   return response.data;
 }
 
 // Route to get article data
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
-    const ntlm = {
-      username: 'ahmed',
-      password: 'ahmed2022',
-      workstation: 'DESKTOP-1EF91E4',
-      domain: 'DESKTOP-1EF91E4'
-    };
-    const articles = await getArticlesFromBC(ntlm);
+    const user = req.ntlm;
+    console.log(user);
+    let articles = await getArticlesFromBC();
     res.json(articles);
   } catch (error) {
     console.error(error);
